a51c5fa8e922482cf90a38197edf2b7ec118c4cb
Alex Nezami <nezami.alireza@courrier.uqam.ca>
2024-01-13 13:30:06 -0500

    main class

---
+++ src/src/ca/uqam/info/mgl7460/boutique/MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+    public static void main(String[] args) {
+        String message = "Réalisation Et maintenance du logiciel TP2 automne 2023 avec Alireza Nezami";
+        System.out.println(message);
+    }
+}
